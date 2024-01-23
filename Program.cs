5697530384f807047b552253e6199ce538a7ca76
Nikiw <Nikiw@NIKIWE_MBHIZA>
2024-01-23 08:56:18 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Variables
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //declaration
+            string name="Nikiwe";
+            int age;
+
+            //initilization
+            name = " Nikiwe";
+            age = 10;
+
+            Console.WriteLine("the was once a girl named" +name);
+            Console.WriteLine(name +"was "+ age+ "age");
+            Console.WriteLine(name + "never really liked her name");
+            Console.ReadKey();
+
+
+        }
+    }
+}
